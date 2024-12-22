95c580ab7d67acaa9dab614911aa324daf2ad8e7
phongnt2005 <phongnguyen.psn@gmail.com>
2024-12-22 16:53:20 +0700

    commit satan clause

---
+++ AtCoder/385B-Santa_Clause1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  int h, w, i, j;
+  std::cin >> h >> w >> i >> j;
+
+  for (int i = 0; i < h; i++) {
+    std::string s;
+    std::cin >> s;
+
+    for (int )
+  }
+}
